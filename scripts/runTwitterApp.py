07480ace52010afe5d76440fc696027d5b98efb3
Lee Mendelowitz <Lee@dhcp490-131.umiacs.umd.edu>
2013-03-04 12:13:29 -0500

    Updated runTwitterApp.py

--- scripts/runTwitterApp.py
+++ scripts/runTwitterApp.py
@@ -7,6 +7,10 @@ OUTPUT_DIR = os.environ.get('OPENSHIFT_DATA_DIR', None)
 if OUTPUT_DIR is None:
     OUTPUT_DIR = os.getcwd()
 
+SCRIPT_DIR = os.environ.get('OPENSHIFT_REPO_DIR', None)
+if SCRIPT_DIR is None:
+    SCRIPT_DIR = os.getcwd()
+
 def main():
 
     logFileName = os.path.join(OUTPUT_DIR, 'runTwitterApp.log')
@@ -22,7 +26,9 @@ def main():
     logFile.write(msg)
     logFile.flush()
 
-    cmd = ['python', 'twitterApp.py', '--test']
+    twitterApp = os.path.join(SCRIPT_DIR, 'twitterApp.py')
+    cmd = ['python', twitterApp, '--test']
+
     p = subprocess.Popen(cmd, stdout = logFile, stderr = logFile)
     ret = p.wait()
 
