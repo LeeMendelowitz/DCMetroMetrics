3fde95e4b2282a3c0032e44278f898a20838e455
Lee Mendelowitz <Lee@dhcp490-131.umiacs.umd.edu>
2013-03-04 15:31:36 -0500

    Updated cleanPickles.sh

--- .openshift/cron/daily/cleanPickles.sh
+++ .openshift/cron/daily/cleanPickles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#find $OPENSHIFT_DATA_DIR -name '*.pickle' -ctime +1d  | xargs -I {} rm {}
-find $OPENSHIFT_DATA_DIR -name '*.pickle' -ctime +1d 
+# Delete pickle data from greater than 24 hours ago
+find $OPENSHIFT_DATA_DIR -name "*.pickle" -mtime +0 | xargs -I {} rm {}
+#find $OPENSHIFT_DATA_DIR -name "*.pickle" -mtime +0
