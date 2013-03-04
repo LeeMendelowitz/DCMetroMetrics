0bd1182ea2efe39065a9870a07d4d2e262082418
Lee Mendelowitz <Lee.Mendelowitz@gmail.com>
2013-03-04 11:25:05 -0500

    Added tweepy dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='YourAppName', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=['greenlet', 'gevent',
+                           'tweepy',
                         #  'MySQL-python',
                         #  'pymongo',
                         #  'psycopg2',
