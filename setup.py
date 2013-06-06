fecabefe42046fca258ebf9f55f86ff615d3c973
Lee Mendelowitz <Lee.Mendelowitz@gmail.com>
2013-06-06 11:40:17 -0400

    Small change to setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,18 @@ setup(name='MetroEscalators', version='1.0',
       description='WMATA Escalator Data',
       author='Lee Mendelowitz', author_email='MetroEscalators@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=['greenlet', 'gevent',
-                           #'tweepy',
                            'requests',
                         #  'MySQL-python',
                            'pymongo',
                         #  'psycopg2',
                            'python-dateutil==1.5',
-                           'oauth2',
-                           'simplejson',
-                           'httplib2',
-                           'python-twitter'
+                           'tweepy',
+#                           'oauth2',
+#                           'simplejson',
+#                           'httplib2',
+#                           'python-twitter'
       ],
      )
